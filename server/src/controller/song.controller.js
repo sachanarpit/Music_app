86834ecd0fb35f64f71df7212f7e636d5e418640
Arpit Sachan <sachanarpitknp@gmail.com>
2022-01-14 19:04:43 +0530

    server 1 api runnig react extra line removed

---
+++ server/src/controller/song.controller.js
@@ -0,0 +1,66 @@
+//import
+const express = require("express");
+const axios = require("axios");
+
+const router = express.Router();
+
+// const getReq = async (url) => {
+//   try {
+//     const data = await axios.get(url);
+//     const jsonData = data.data;
+//     return jsonData;
+//   } catch (err) {
+//     return err;
+//   }
+// };
+// console.log(
+//   getReq(`https://saavn.me
+// /search?album=rockstar`)
+// );
+
+// //routes
+// router.get("", async (req, res) => {
+//   return res.status(200).send("hello");
+// });
+
+async function fetchData(name) {
+  const response = await axios.get(`https://saavn.me/search?album=${name}`);
+  return response.data;
+}
+
+router.get("/", async (req, res) => {
+  try {
+    const arrOfArtist = [
+      "Badshah",
+      "arijit",
+      "pritam",
+      "Vishal",
+      "Shaan",
+      "Jubin",
+      "Jasleen",
+      "Various Artists",
+    ];
+
+    const response1 = await fetchData(arrOfArtist[0]);
+    const response2 = await fetchData(arrOfArtist[1]);
+    const response3 = await fetchData(arrOfArtist[2]);
+    const response4 = await fetchData(arrOfArtist[3]);
+    const response5 = await fetchData(arrOfArtist[4]);
+    const response6 = await fetchData(arrOfArtist[5]);
+    const response7 = await fetchData(arrOfArtist[6]);
+    let output = [
+      ...response1,
+      ...response2,
+      ...response3,
+      ...response4,
+      ...response5,
+      ...response6,
+      ...response7,
+    ];
+    res.status(200).json(output);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+module.exports = router;
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const songcontroller = require("./controller/song.controller");
 
 const app = express();
 const port = process.env.PORT;
@@ -14,6 +15,7 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/song", songcontroller);
 
 //server
 const start = () => {
